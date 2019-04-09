0ea8f0dfb47d8d668ab7fec9bb8f5be61937e4c8
Jerry Evarts <jerryevarts@gmail.com>
2019-04-08 19:36:49 -0700

    Update data.js

--- backend/data.js
+++ backend/data.js
@@ -11,7 +11,22 @@ const DataSchema = new Schema(
     { timestamps: true }
 );
 
+//this is the first form's data structure
+
+const FormDataSchema = new Schema(
+    {
+        id: Number,
+        name: String,
+        age: Number,
+        gender: String,
+        skills: Array, // maybe change this
+        about: String
+    },
+    { timestamps: true }
+);
+
+
 // export the new Schema so we could modify it using Node.js
 module.exports = mongoose.model("Data", DataSchema);
-
+module.exports = mongoose.model("Form", FormDataSchema);
 
